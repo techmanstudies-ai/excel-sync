c41e4da55c73e09bfea83d661e9e6b1e7f452289
techmanstudies-ai <techmanstudies@gmail.com>
2026-04-10 15:33:10 +0800

    Update sync.py

--- sync.py
+++ sync.py
@@ -6,9 +6,9 @@ import json
 from datetime import datetime, timezone
 from google.oauth2.service_account import Credentials
 
-# ===============================
+# ==============================
 #  CONFIG (FROM GITHUB SECRETS)
-# ===============================
+# ==============================
 
 TENANT_ID = os.environ["TENANT_ID"]
 CLIENT_ID = os.environ["CLIENT_ID"]
