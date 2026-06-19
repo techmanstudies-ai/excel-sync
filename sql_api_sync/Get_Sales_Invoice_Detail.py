a532ffd0d63780511c98fb2f322caad30793ca94
techmanstudies-ai <techmanstudies@gmail.com>
2026-06-19 10:10:25 +0800

    Update Get_Sales_Invoice_Detail.py

--- sql_api_sync/Get_Sales_Invoice_Detail.py
+++ sql_api_sync/Get_Sales_Invoice_Detail.py
@@ -52,7 +52,7 @@ scope = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+creds_dict = json.loads(os.environ["SQL_GOOGLE_CREDENTIALS"])
 
 creds = Credentials.from_service_account_info(
     creds_dict,
